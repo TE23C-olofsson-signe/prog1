ebe0656477922af9ae17a1dcdf233549afec5953
Signe Olofsson <signe.olofsson@elev.ga.ntig.se>
2024-08-30 13:58:02 +0200

    första

---
+++ intro/luck-text/Program.cs
@@ -0,0 +1,27 @@
+string name;
+string colour; 
+string music;
+
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Clear(); 
+
+Console.WriteLine("Skriv dit namn");
+name= Console.ReadLine(); 
+
+// Console.WriteLine("Hello " + name +"!");
+Console.WriteLine($"Hello{name}!");
+
+Console.WriteLine("Skriv din favorit färg");
+colour= Console.ReadLine();
+
+Console.WriteLine("favorit låt");
+music= Console.ReadLine();
+
+
+
+
+Console.WriteLine($"Hej{name}! jag ser att du gillar {colour} Jag ser att du gillar {music}");
+
+Console.ReadLine(); 
